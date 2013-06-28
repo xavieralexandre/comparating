8f87ce26a0a4a53937ed1dd96bc2671ab42df685
Xavier Alexandre <cuocoli@gmail.com>
2013-06-28 22:43:58 +0200

    Added data to the items collection (List of ice-cream flavors and starting score)

---
+++ server/fixtures.js
@@ -0,0 +1,370 @@
+if (Items.find().count() === 0) {
+  Items.insert({
+    title: 'Vanilla Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Vanilla Ice Cream, Philadelphia-Style',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Chocolate Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Chocolate Ice Cream, Philadelphia-Style',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Aztec “Hot” Chocolate Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Chocolate–Peanut Butter Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Chocolate-Raspberry Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Milk Chocolate Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Guinness–Milk Chocolate Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'White Chocolate Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Coffee Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Coffee Frozen Yogurt',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Viettitlese Coffee Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Anise Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Cinnamon Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Black Currant Tea Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Green Tea Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Kinako Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Fresh Ginger Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Butterscotch Pecan Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Date, Rum, and Pecan Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Gianduja Gelato',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Maple Walnut Ice Cream with Wet Walnuts',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Vanilla Frozen Yogurt',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Peanut Butter Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Orange Popsicle Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Malted Milk Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Oatmeal-Raisin Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Rum Raisin Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Tin Roof Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Zabaglione Gelato',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Chartreuse Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Eggnog Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Crème Fraîche Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Toasted Almond and Candied Cherry Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Goat Cheese Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Cheesecake Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Tiramisù Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Lavender-Honey Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Roquefort-Honey Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Turrón Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Sweet Potato Ice Cream with Maple-Glazed Pecans',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Panforte Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Rice Gelato',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Roasted Banana Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Sour Cherry Frozen Yogurt',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Dried Apricot–Pistachio Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Fresh Apricot Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Plum Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Prune-Armagnac Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Pear-Caramel Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Fresh Fig Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Pear-Pecorino Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Olive Oil Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Orange–Szechwan Pepper Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Super Lemon Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Lemon-Speculoos Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Blueberry Frozen Yogurt',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Peach Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Peach Frozen Yogurt',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Strawberry–Sour Cream Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Strawberry Frozen Yogurt',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Raspberry Swirl Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Raspberry Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Passion Fruit Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Avocado Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Toasted Coconut Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Green Pea Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Fresh Mint Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Basil Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Parsley Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Black Pepper Ice Cream',
+	score: 0
+  });
+
+  Items.insert({
+    title: 'Saffron Ice Cream'
+}
+];
+Template.itemsList.helpers({
+items: itemsData
+});
+
+}
